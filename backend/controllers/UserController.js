b36d5aaa838069fe32d5d0aac2708e46a3a3d2b6
EduardoGrillo <https://github.com/EduardoGrillo>
2024-02-02 07:52:37 -0300

    erro corrigido

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -95,7 +95,7 @@ const update = async (req, res) => {
 
     const reqUser = req.user;
 
-    const user = await User.findById(mongoose.Types.ObjectId(reqUser._id)).select("-password");
+    const user = await User.findById(new mongoose.Types.ObjectId(reqUser._id)).select("-password");
 
     if (name) {
         user.name = name;
@@ -124,7 +124,7 @@ const update = async (req, res) => {
 const getUserById = async (req, res) => {
     const { id } = req.params;
 
-    const user = await User.findById(mongoose.Types.ObjectId(id)).select(
+    const user = await User.findById(new mongoose.Types.ObjectId(id)).select(
         "-password"
     );
 
