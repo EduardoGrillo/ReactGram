0d7b9dd8597131bf6083fe5c5765c06b0950c96a
EduardoGrillo <https://github.com/EduardoGrillo>
2024-02-02 09:45:06 -0300

    delete photo

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -23,14 +23,44 @@ const insertPhoto = async (req, res) => {
 
     // if photo was create successfully, return data
     if (!newPhoto) {
-        errors: ["Houve um problema, por favor tente novamente mais tarde."]
+        res.status(422).json({
+            errors: ["Houve um problema, por favor tente novamente mais tarde."],
+        });
+
+        return;
     }
 
     res.status(201).json(newPhoto)
-
-    res.send("Photo insert");
 };
+
+// remove a photo from db
+const deletePhoto = async (req, res) => {
+    const { id } = req.params
+
+    const reqUser = req.user
+
+    const photo = Photo.findById(new mongoose.Types.ObjectId(id))
+
+    // check if photo exists
+    if (!photo) {
+        res.status(404).json({ errors: ["Foto não encontrada."] })
+
+        return;
+    }
+
+    // check photo belongs to user
+    if (!photo.userId.equals(reqUser._id)) {
+        res.status(422).json({
+            errors: ["Ocorreu um erro, por favor tente novamente mais tarde."]
+        })
+    }
+
+    await Photo.findByIdAndDelete(photo._id)
+
+    res.status(200).json({ id: photo._id, message: "Foto excluída com sucesso." })
+}
 
 module.exports = {
     insertPhoto,
+    deletePhoto,
 }
